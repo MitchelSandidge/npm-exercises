22f484d6b49c12a56a601fbcc54ac344f984526c
Mitchel Sandidge <masandidge12@gmail.com>
2020-03-12 14:38:29 -0500

    added npm

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const _ = require("lodash");
+const $ = require('jquery');
+
+const sayHello = (()=> {
+    console.log("Hello world")});
+
+sayHello();
+
+
+$('body').css('background-color', 'red');
